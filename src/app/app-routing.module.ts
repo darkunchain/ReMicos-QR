777ba2087ffa174a7f9c48dbd48f486447aa7403
darkunchain <luisfer74081@gmail.com>
2022-02-17 09:01:46 -0500

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { RedimirComponent } from './components/redimir/redimir.component';
 
 const routes: Routes = [
   { path: 'home', component: FormComponent },
-  { path: '', component: FormComponent },
   { path: 'qrgen', component: GenQRComponent },
   { path: 'redime', component: RedimirComponent },
   { path: 'qrgen/:id', redirectTo: '/redime', pathMatch: 'prefix' },
   { path: '**', redirectTo: '/redime' },
+  { path: '', component: FormComponent },
 ];
 
 @NgModule({
